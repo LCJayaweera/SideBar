f8906da12e6decd14868219ea52f5a11e5f85c94
Lakshan <lcjayaweera@gmail.com>
2023-07-06 21:04:36 +0900

    GlobalContext Setup

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useGlobalContext } from './Context'
 
 const Home = () => {
+const {openSidebar,openModal}=useGlobalContext();
+console.log(openSidebar,openModal)
+
   return (
     <div>
       Home
