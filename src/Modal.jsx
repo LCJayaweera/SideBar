ff73d6fe3ae8a532990a6661de9baecbf488f307
Lakshan <lcjayaweera@gmail.com>
2023-07-06 22:20:01 +0900

    Modal close open funtion Added

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -3,8 +3,8 @@ import {FaTimes} from 'react-icons/fa';
 import { useGlobalContext } from './Context';
 
 const Modal = () => {
-  const {isModal,closeModal}=useGlobalContext();
-  return (<div className='modal-overlay'>
+  const { isModalOpen,closeModal}=useGlobalContext();
+  return (<div className={ isModalOpen?"modal-overlay show-modal":"modal-overlay"}>
       <div className="modal-container">
         <h3>modal content</h3>
         <button className='close-modal-btn' onClick={closeModal}>
